36d947d73ffb3dbf861033d5704fcf38b05f0950
rvbug <rakesh.venkat@gmail.com>
2026-03-10 21:18:20 +0530

    Rustimate brew formula

---
+++ Formula/rustimate.rb
@@ -0,0 +1,15 @@
+class Rustimate < Formula
+  desc "Cinematic DSL renderer for code and terminal videos"
+  homepage "https://rustimate.io"
+  url "https://github.com/rustimate/rustimate/releases/download/v1.0/rustimate-macos.tar.gz"
+  sha256 "c9683b8c3c3b6ba1cb45cd74dbc384040f53b79f7b377cf07339be1d9b5d4aee"
+  license "Commercial"
+
+  def install
+    bin.install "rustimate"
+  end
+
+  test do
+    system "#{bin}/rustimate", "--version"
+  end
+end
