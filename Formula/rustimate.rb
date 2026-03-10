207b729733c64e3a1bac49af90b3ce6d38cf9600
rvbug <rakesh.venkat@gmail.com>
2026-03-10 21:39:02 +0530

    Update release URL

--- Formula/rustimate.rb
+++ Formula/rustimate.rb
@@ -1,7 +1,7 @@
 class Rustimate < Formula
   desc "Cinematic DSL renderer for code and terminal videos"
   homepage "https://qubitai.in/rustimate.html`"
-  url "https://github.com/rustimate/releases/download/v0.1.0/rustimate-macos.tar.gz"
+  url "https://github.com/rustimate/rustimate/releases/download/v0.1.0/rustimate-macos.tar.gz"
   sha256 "c9683b8c3c3b6ba1cb45cd74dbc384040f53b79f7b377cf07339be1d9b5d4aee"
   license "Commercial"
 
