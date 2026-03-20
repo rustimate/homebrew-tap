40a1066a4db0b143a258d95856840f70853975ed
rvbug <rakesh.venkat@gmail.com>
2026-03-20 15:43:56 +0530

    Update checksum

--- Formula/rustimate.rb
+++ Formula/rustimate.rb
@@ -6,18 +6,18 @@ class Rustimate < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/rustimate/rustimate/releases/download/v0.1.0/rustimate-macos-aarch64.tar.gz"
-      sha256 "ac62178db5098f818a448254b8030656429c071203858a1aa50495ab89a7d77c"
+      sha256 "fdcb1c1be43acc31532f8e6f2fdd5685a281d85ad17abcdd3f1bef8e3d56306a"
     else
       url "https://github.com/rustimate/rustimate/releases/download/v0.1.0/rustimate-macos-x86_64.tar.gz"
-      sha256 "5c7165bf82d77cf5affa8eea469b3f74f2f11882a6ae3d143e5a26a0b39e93b4"
+      sha256 "3ee68694b62b6a9d076035a45bd0fbf70a57f94c76eee3d07c505a37c469593c"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/rustimate/rustimate/releases/download/v0.1.0/rustimate-linux-aarch64.tar.gz"
-      sha256 "3f8262e6601194fda806e0207f08f257d4b4c638a7cdb864f4ca1c8daac54cb8"
+      sha256 "8255a7597624b9d46b17ea0befe92eeb1fa037e8e019fbf2bd0b813c63240fe1"
     else
       url "https://github.com/rustimate/rustimate/releases/download/v0.1.0/rustimate-linux-x86_64.tar.gz"
-      sha256 "9425ad4af9f38d20f06a0538744ab13f6a6fefc0021e1b409b70d5e3f2c65bd2"
+      sha256 "34a0822c99cda51566853183fa27cb049ce8f67cd9a0486ec83ae8725825904d"
     end
   end
 
